2ab64178a48b22107fcf7f8aef9fdd543a3e7c1a
waltonR1 <waltonR1@163.com>
2025-05-27 04:11:13 +0200

    init

---
+++ src/main/java/com/isep/vibelink/VibeLinkApplication.java
@@ -0,0 +1,13 @@
+package com.isep.vibelink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibeLinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VibeLinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isep/vibelink/VibeLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isep.vibelink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VibeLinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
